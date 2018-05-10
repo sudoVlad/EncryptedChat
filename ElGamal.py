8d23d77b53704dd5131f1e0a3e79cd34bc43c4f0
Vladimir Beauge <vladBeauge@gmail.com>
2018-05-09 23:51:07 -0400

    added comments to ElGamal

--- ElGamal.py
+++ ElGamal.py
@@ -9,8 +9,10 @@ class ElGamal:
 		self.prime = prime
 		self.halfMask = halfMask
 		self.g = g
-
-	#retuns an array of ascii numbers to represent the cipher
+	
+	# Encrypts each character in the message
+	# Parameters: a, msg
+	# Retuns an array of ascii numbers to represent the cipher
 	def encrypt(a, msg):
 		fullMask = findFullMask(a)	
 		#encrypt each letter		
@@ -19,8 +21,10 @@ class ElGamal:
 			msg[x] = convert2ASCII(msg[x])							
 			cipher.append(findCipher(msg[x],fullMask))				
 		return cipher
-
-	#returns an array of characters to represent the message
+	
+	# Decrypts each character in the cipher
+	# Parameters: b, cipher
+	# Returns an array of characters to represent the message
 	def decrypt(b, cipher):
 		decrypt each letter
 		fullMask = findFullMask(b)
@@ -29,23 +33,33 @@ class ElGamal:
 			cipher[x] = findMessage(cipher[x],fullMask)			
 		 	msg.append(convert2Char(cipher[x]))			
 	
-	#returns the halfmask raised to the secret value of the caller
+	# Finds the full mask
+	# Parameters: a
+	# Returns the halfmask raised to the secret value of the caller
 	def findFullMask(a):
 		return math.pow(self.halfMask,a) % self.prime				
 	
-	#returns the ascii represetnation of message and the full mask
+	# Finds the cipher
+	# Parameters: asciiNum, fullMask
+	# Returns the ascii represetnation of message and the full mask
 	def findCipher(asciiNum, fullMask):
 		return asciiNum * fullMask % self.prime
-
-	#returns the product of the cipher and inverted full mask
+	
+	# Finds the message
+	# Parameters: asciiNum, fullMask
+	# Returns the product of the cipher and inverted full mask
 	def findMessage(asciiNum, fullMask):
 		invFullMask = Pulverizor(self.prime, fullMask)
 		return asciiNum * invFullMask % prime
 
-	#returns ascii number of character
+	# Converst the ascii number to characters
+	# Parameters: letter
+	# Returns ascii number of character
 	def convert2ASCII(letter):
 		return ord(letter)
 	
-	#returns the character version of ascii number
+	# Converts the characters to ascii number
+	# Parameters: asciiNum
+	# Returns the character version of ascii number
 	def convert2Char(asciiNum):
 		return chr(asciiNum)
